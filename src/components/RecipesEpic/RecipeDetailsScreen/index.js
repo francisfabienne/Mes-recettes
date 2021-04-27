2d8cbb0a308b8b50adcccf5292d6750de97b877d
francisfabienne <57148319+francisfabienne@users.noreply.github.com>
2021-04-28 00:26:19 +0200

    Update index.js

--- src/components/RecipesEpic/RecipeDetailsScreen/index.js
+++ src/components/RecipesEpic/RecipeDetailsScreen/index.js
@@ -18,12 +18,15 @@ const RecipesDetailsScreen = ( {navigation, route} ) => {
       <View style={{height: 300, padding: 10, backgroundColor: 'black'}}>
          <Image source = {{uri: RecetteInfo.image}} style = {{ height: 315, width: '100%'}} resizeMode="contain"/>
          <Text style={{fontWeight: 700, marginBottom: 25, color: 'black', textAlign: 'center', fontSize: 24}}>{RecetteInfo.title}</Text>
+        {/*} <FlatList
+              data= {RecetteInfo}
+              renderItem = {(item) => {
+                  return <Text>{RecetteInfo.extendedIngredients.name}</Text>
+              }}
+            />   */}
+         
       </View>
-      {/* <View style={{paddingTop: 25}}>
-        {RecetteInfo.extendedIngredients.map((item) => {
-          return <Text>{item.name}</Text>
-        })}
-      </View> */}
+     
     </View>
   );
 };
